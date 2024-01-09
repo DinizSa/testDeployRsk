9e33ea03dbacec0f2fe063974b8fb1cdb40660fe
DinizSa <39380608+DinizSa@users.noreply.github.com>
2024-01-09 10:18:21 +0000

    cleanup

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,20 +2,9 @@ import { ethers } from "hardhat";
 require("dotenv").config({ path: ".env" });
 
 async function main() {
-  //   const factory = await ethers.getContractFactory("TestA");
-  //   const testA = await factory.deploy();
+  const mockContract = await (await ethers.deployContract("mockContract")).waitForDeployment();
 
-  const signers = await ethers.getSigners();
-  const owners: string[] = signers
-    .slice(0, 2)
-    .map((signer) => signer.address.toLowerCase());
-  const requiredSignatures = 2;
-
-  const multiSigWallet = await ethers.deployContract("mockContract");
-
-  // Wait for the contract to be deployed
-  await multiSigWallet.waitForDeployment();
-  console.log("multiSigWallet address:", await multiSigWallet.getAddress());
+  console.log("mockContract address:", await mockContract.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
